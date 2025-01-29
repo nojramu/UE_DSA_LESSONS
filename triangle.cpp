753cb46cf334b1f179f277b515c9ce077bde9970
marjon umbay <97776511+nojramu@users.noreply.github.com>
2025-01-29 10:09:34 +0000

    triangles

--- triangle.cpp
+++ triangle.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    int height = 5; // You can change the height of the triangle here
+    int height = 10; // You can change the height of the triangle here
 
     for (int i = 1; i <= height; ++i) {
         for (int j = 1; j <= height - i; ++j) {
