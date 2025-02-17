073cbc358c0510de8e7d0ecf536efe008e1e38de
marjon umbay <97776511+nojramu@users.noreply.github.com>
2025-02-17 00:46:31 +0000

    created func1

---
+++ func1v1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int sum (); // FUNCTION DECLARATION
+int diff ();
+void print();
+
+int main () {
+    int total, result;
+    result = diff();
+    total = sum();
+
+    cout << "The difference of num1 to num2 =" <<result << endl;
+    cout << "The sum of num3 and num4 =" <<total << endl;
+    print();
+    //system("pause"); 
+}
+
+int diff () {
+    int num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    return num1 - num2;
+}
+
+int sum () { // FUNCTION DEFINITION
+    int num3, num4;
+    cout << "Enter two numbers: ";
+    cin >> num3 >> num4;
+    return num3 + num4;
+}
+
+void print () {
+    cout << "[A] Sum" << endl;
+    cout << "[B] Difference" << endl;
+    cout << "[C] Product" << endl;
+    cout << "[D] Quit" << endl;
+}
