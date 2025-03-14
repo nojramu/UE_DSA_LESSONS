7291ba3887866ff83525407457c39e0a08c94fdb
marjon umbay <97776511+nojramu@users.noreply.github.com>
2025-03-14 02:29:41 +0000

    improve merge sort

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 void insertionSort(vector<int>& arr) {
     int n = arr.size();
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];
+    for (int i = 1; i < n; i++) { //2nd to the last element
+        int key = arr[i]; //assigns the current element to the variable key
         int j = i - 1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
+        while (j >= 0 && arr[j] > key) { // it will continue as long as the j is non-negative
+            arr[j + 1] = arr[j]; //use toshift the element
             j--;
         }
         arr[j + 1] = key;
@@ -17,7 +17,7 @@ void insertionSort(vector<int>& arr) {
 }
 
 void printArray(const vector<int>& arr) {
-    for (int num : arr) {
+    for (int num : arr) { //range based 
         cout << num << " ";
     }
     cout << endl;
--- mergeSort.cpp
+++ mergeSort.cpp
@@ -3,50 +3,73 @@
 
 using namespace std;
 
+// Function to merge two subarrays of arr.
+// First subarray is arr[left..mid]
+// Second subarray is arr[mid+1..right]
 void merge(vector<int>& arr, int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
+    int n1 = mid - left + 1; // Size of the first subarray
+    int n2 = right - mid;    // Size of the second subarray
 
-    vector<int> L(n1), R(n2);
+    // Create temporary arrays
+    vector<int> L(n1);
+    vector<int> R(n2);
 
-    for (int i = 0; i < n1; i++)
+    // Copy data to temporary arrays L[] and R[]
+    for (int i = 0; i < n1; ++i)
         L[i] = arr[left + i];
-    for (int j = 0; j < n2; j++)
+    for (int j = 0; j < n2; ++j)
         R[j] = arr[mid + 1 + j];
 
+    // Merge the temporary arrays back into arr[left..right]
     int i = 0, j = 0, k = left;
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
             arr[k] = L[i];
-            i++;
+            ++i;
         } else {
             arr[k] = R[j];
-            j++;
+            ++j;
         }
-        k++;
+        ++k;
     }
 
+    // Copy the remaining elements of L[], if any
     while (i < n1) {
         arr[k] = L[i];
-        i++;
-        k++;
+        ++i;
+        ++k;
     }
 
+    // Copy the remaining elements of R[], if any
     while (j < n2) {
         arr[k] = R[j];
-        j++;
-        k++;
+        ++j;
+        ++k;
     }
 }
 
+// Function to implement merge sort
+// arr is the array to be sorted
+// left is the starting index
+// right is the ending index
 void mergeSort(vector<int>& arr, int left, int right) {
-    if (left >= right)
-        return;
+    if (left < right) {
+        // Find the middle point
+        int mid = left + (right - left) / 2;
 
-    int mid = left + (right - left) / 2;
-    mergeSort(arr, left, mid);
-    mergeSort(arr, mid + 1, right);
-    merge(arr, left, mid, right);
+        // Sort first and second halves
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        // Merge the sorted halves
+        merge(arr, left, mid, right);
+    }
+}
+
+void printArray(const vector<int>& arr) {
+    for (int num : arr)
+        cout << num << " ";
+    cout << endl;
 }
 
 int main() {
@@ -54,16 +77,12 @@ int main() {
     int arr_size = arr.size();
 
     cout << "Given array is \n";
-    for (int i = 0; i < arr_size; i++)
-        cout << arr[i] << " ";
-    cout << endl;
+    printArray(arr);
 
     mergeSort(arr, 0, arr_size - 1);
 
     cout << "\nSorted array is \n";
-    for (int i = 0; i < arr_size; i++)
-        cout << arr[i] << " ";
-    cout << endl;
+    printArray(arr);
 
     return 0;
 }
