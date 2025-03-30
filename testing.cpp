baf7505ffab650b0f4a36882bfd20d5c97884f0a
marjon umbay <97776511+nojramu@users.noreply.github.com>
2025-03-30 23:30:37 +0000

    testing for computer programming

---
+++ testing.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+char letter = 'c';
+
+
+int  main(){
+    for (int i = 0; i < 10; i++){
+        cout << "Hello World" << endl;
+    }
+    int j = 0;
+    while (j<5){
+        cout << "no" << endl;
+        j++;
+    }
+    int k = 0;
+    do {
+        cout << "yes" << endl;
+        k++;
+    } while (k<10);
+
+}
